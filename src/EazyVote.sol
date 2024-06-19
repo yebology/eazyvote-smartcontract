5ef8c0e3425b673cc395b7c6e1ba5ad52c2ce1da
yebology <yobelnathaniel12@gmail.com>
2024-06-19 09:03:11 +0700

    done custom error, event, modifier, function createNewElection, and addNewCandidate

--- src/EazyVote.sol
+++ src/EazyVote.sol
@@ -9,7 +9,8 @@ contract EazyVote {
     }
 
     struct Candidate {
-        uint256 voteCount;
+        uint256 id;
+        address[] voter;
         string candidateName;
         string candidatePhoto;
         string candidateVision;
@@ -28,7 +29,34 @@ contract EazyVote {
     uint256 private totalElection;
     Election[] private elections;
 
-    modifier onlyVoteOneTimeInOneElection(uint256 userId) {
+    error ElectionIsNotOpen(uint256 electionId);
+    error VoterAlreadyVote(address voter, uint256 electionId);
+
+    event newElectionHasBeenCreated(uint256 indexed electionId);
+    event newCandidateHasBeenAdded(uint256 indexed electionId, uint256 indexed candidateId);
+    event voterHasBeenVoted(address indexed voter, uint256 electionId, uint256 indexed candidateId);
+
+    modifier onlyVoteOneTimeInOneElection(address voter, uint256 electionId) {
+        Election memory election = elections[electionId];
+        Candidate[] memory candidates = election.candidates;
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (candidates[i].voter.length >= 1) {
+                for (uint256 j = 0; j < candidates[i].voter.length; j++) {
+                    address candidateVoter = candidates[i].voter[j];
+                    if (candidateVoter == voter) {
+                        revert VoterAlreadyVote(voter, electionId);
+                    }
+                }
+            }
+        }
+        _;
+    }
+
+    modifier electionMustStillOpen(uint256 electionId) {
+        Election memory election = elections[electionId];
+        if (election.electionStatus == Status.CLOSED) {
+            revert ElectionIsNotOpen(electionId);
+        }
         _;
     }
 
@@ -36,12 +64,24 @@ contract EazyVote {
         owner = msg.sender;
     }
 
-    function createElection(
+    function createNewElection(
         uint256 electionStart,
         uint256 electionEnd
-    ) public {
-
+    ) external {
+        Election memory newElection;
+        newElection.id = elections.length;
+        newElection.electionStart = electionStart;
+        newElection.electionEnd = electionEnd;
+        newElection.electionStatus = Status.CLOSED;
+        elections.push(newElection);
+        totalElection += 1;
     }
+
+    function changeElectionStatus(
+        uint256 electionId,
+        uint256 electionStart,
+        uint256 electionEnd
+    ) public {}
 
     function addNewCandidate(
         uint256 electionId,
@@ -49,18 +89,18 @@ contract EazyVote {
         string memory candidatePhoto,
         string memory candidateVision,
         string memory candidateMission
-    ) public {
-
+    ) external {
+        Candidate memory newCandidate;
+        newCandidate.candidateName = candidateName;
+        newCandidate.candidatePhoto = candidatePhoto;
+        newCandidate.candidateVision = candidateVision;
+        newCandidate.candidateMission = candidateMission;
+        elections[electionId].candidates.push(newCandidate);
     }
 
-    function voteCandidate(
-        uint256 electionId,
-        uint256 candidateId
-    ) public {
+    function voteCandidate(uint256 electionId, uint256 candidateId) public {}
 
-    }
-
-    function getElections() external view returns(Election[]) {
+    function getElections() external view returns (Election[] memory) {
         return elections;
     }
     //
